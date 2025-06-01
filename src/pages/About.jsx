48a5ff876b541f2fed042fcc2a48e070e264a665
Arpit Bharadwaj <89962692+arpitbharadwaj1@users.noreply.github.com>
2025-06-01 19:15:29 +0530

    - Added about page logic - Added custom data to showcase in FE - Refactored code

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import countryData from "../api/countryData.json";
 
 const About = () => {
   return (
-    <div>
-        <h1>About Us</h1>
-    </div>
-  )
-}
+    <section className="section-about container">
+      <h2 className="container-title">
+        Here are the intresting facts <br /> about the world and its countries.
+      </h2>
 
-export default About
+      <div className="gradient-cards">
+        {countryData.map((country) => {
+          const { id, countryName, capital, population, interestingFact } =
+            country;
+          return (
+            <div className="card" key={id}>
+              <div className="container-card bg-blue-box">
+                <p className="card-title">{countryName}</p>
+                <p>
+                  <span className="card-description">Capital:</span>
+                  {capital}
+                </p>
+                <p>
+                  <span className="card-description">Population:</span>
+                  {population}
+                </p>
+                <p>
+                  <span className="card-description">Interesting Fact:</span>
+                  {interestingFact}
+                </p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default About;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,13 @@
-import React from "react";
-import { FaLongArrowAltRight } from "react-icons/fa";
+import HeroSection from "../components/UI/HeroSection";
+import About from "./About";
 
 const Home = () => {
   return (
-    <main>
-      <div className="hero-section main">
-        <div className="container grid grid-two-cols">
-          <div className="her-content">
-            <h1 className="heading-xl">
-              Explore the world, One country at a time
-            </h1>
-            <p className="paragraph">
-              Discover the history, culture, and beauty of every nation. Sort,
-              search, and filter through countries to find the details you need.
-              Whether you're planning a trip, studying geography, or just
-              curious about the world, our platform has you covered.
-            </p>
-            <button className="btn btn-darken btn-inline bg-white-box">
-              Start Exploring <FaLongArrowAltRight />
-            </button>
-          </div>
-          <div className="hero-image"></div>
-        </div>
-      </div>
-    </main>
+    <>
+    <HeroSection/>
+    <About />
+    </>
+
   );
 };
 
